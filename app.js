26da22432e7548edef95c5b8b377c58c9ee6cc74
YVeselovskyi <19dynamokyiv94@gmail.com>
2016-10-30 16:37:57 +0200

    Markers from database

--- app.js
+++ app.js
@@ -22,13 +22,18 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+
 app.use('/', express.static(__dirname + '/public/assets'));
 
 io.on('connection', function(socket){
 
-    socket.on('my other event', function (data) {
+    Visitor.find({}, function (err, docs) {
+        socket.emit('all visitors', { visitors: docs });
+    });
 
-        const user = new Visitor({ name: 'Kubik', coordinates: {lat: data.lat, lon: data.lon} });
+    socket.on('new visitor', function (data) {
+
+        const user = new Visitor({ name: 'Marker', coordinates: {lat: data.lat, lng: data.lng} });
 
         user.save(function (err) {
             if (err) {
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,14 +1,41 @@
 'use strict';
 const socket = io();
 
+socket.on('all visitors', function (data) {
+    let allVisitors = data.visitors;
+
+    allVisitors.forEach( function (i) {
+        createMarker(i.coordinates.lat, i.coordinates.lng);
+    })
+});
+
 const options = {
     enableHighAccuracy: true
 };
 
+let map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
+function createMarker(lattitude, longitude) {
+    var marker = new google.maps.Marker({
+        position: {lat: lattitude, lng: longitude},
+        map: map,
+        title: 'Hello World!'
+    });
+}
+
 function success(pos) {
     let crd = pos.coords;
 
-    socket.emit('my other event', { lat: crd.latitude , lon: crd.longitude });
+    socket.emit('new visitor', { lat: crd.latitude , lng: crd.longitude });
+
+    createMarker(crd.latitude, crd.longitude );
+
 }
 
 function error(err) {
@@ -16,4 +43,10 @@ function error(err) {
 }
 
 navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+
+
+
+
 
